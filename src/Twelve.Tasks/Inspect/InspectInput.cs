060ff8bd1fc4a6b3778bcd12a01059fe587dd93f
andy <andydavies2020@gmail.com>
2018-06-08 22:13:42 +0300

    12: add two commands

---
+++ src/Twelve.Tasks/Inspect/InspectInput.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization.Json;
+using System.Threading.Tasks;
+using Oakton;
+
+namespace Twelve.Tasks.Inspect
+{
+	public class InspectInput
+	{
+		public Things Thing { get; set; }
+
+		public bool StatsFlag { get; set; }
+	}
+
+	public enum Things
+	{
+		Users,
+		Images
+	}
+
+	[Description("Inspects various items stored by the service")]
+	public class InspectCommand : OaktonAsyncCommand<InspectInput>
+	{
+		public InspectCommand()
+		{
+			Usage("Count things").Arguments(x => x.Thing).ValidFlags(x => x.StatsFlag);
+		}
+
+		public override async Task<bool> Execute(InspectInput input)
+		{
+			await Console.Out.WriteLineAsync(input.Thing.ToString());
+
+			if (input.StatsFlag)
+				await Console.Out.WriteLineAsync("No stats :(");
+
+			return true;
+		}
+	}
+}
--- src/Twelve.Tasks/Program.cs
+++ src/Twelve.Tasks/Program.cs
@@ -1,12 +1,21 @@
-﻿using System;
+﻿using System.Reflection;
+using System.Threading.Tasks;
+using Oakton;
+using Oakton.Help;
 
 namespace Twelve.Tasks
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	class Program
+	{
+		static async Task<int> Main(string[] args)
+		{
+			return await CommandExecutor
+				.For(_ =>
+				{
+					_.RegisterCommands(typeof(Program).Assembly);
+					_.DefaultCommand = null;
+				})
+				.ExecuteAsync(args);
+		}
+	}
 }
