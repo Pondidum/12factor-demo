aad865135d54dfb757a8f4a80ecc11d9cc1f3924
andy <andydavies2020@gmail.com>
2018-06-08 21:33:41 +0300

    12: add blank tasks project

---
+++ src/Twelve.Tasks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Twelve.Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
